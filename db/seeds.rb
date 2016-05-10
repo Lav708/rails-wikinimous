7304137bedf8e6475f80a539b5aef66cc6315918
Guillaume Lavoisier <lavoisierguillaume@gmail.com>
2016-05-10 16:43:42 +0200

    features added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+# Creating 100 fake restaurants
+10.times do
+  article = Article.new({
+    title: Faker::Hipster.sentence,
+    content: Faker::Hipster.paragraph,
+  })
+  article.save
+end
